315937394f638c0d87b650509600668d7078d645
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-15 18:17:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var animals = ["cat", "lion", "dog"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log(animal);
+}
+console.log("\n");
+for (var _a = 0, animals_2 = animals; _a < animals_2.length; _a++) {
+    var animal = animals_2[_a];
+    console.log("A $(animals) has a tall");
+}
+console.log("\n all of these are great petsl but  i love cats more");
---
+++ main.ts
@@ -0,0 +1,12 @@
+let animals : string [] = ["cat", "lion", "dog"]
+
+for (let animal of animals) {
+    console.log(animal)
+}
+console.log ("\n")
+
+for (let animal of animals){
+    console.log ( "A $(animals) has a tall")
+}
+
+console.log ("\n all of these are great petsl but  i love cats more")
